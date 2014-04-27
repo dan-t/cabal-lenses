7e84b3f485871951fa05e0659c3bd0c7f2fbb9a8
Daniel Trstenjak <daniel.trstenjak@gmail.com>
2014-04-27 19:15:28 +0200

    Add CabalLenses module

---
+++ lib/CabalLenses.hs
@@ -0,0 +1,14 @@
+
+module CabalLenses
+   ( module CabalLenses.CondVars
+   , module CabalLenses.PackageDescription
+   , module CabalLenses.Section
+   , module CabalLenses.Traversals.BuildInfo
+   , module CabalLenses.Traversals.Dependency
+   ) where
+
+import CabalLenses.CondVars
+import CabalLenses.PackageDescription
+import CabalLenses.Section
+import CabalLenses.Traversals.BuildInfo
+import CabalLenses.Traversals.Dependency
